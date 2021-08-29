ebea8600a546915f7f93b8b60c675c494b300857
Lyra Raudales <jraudales2@huskers.unl.edu>
2021-08-29 03:58:24 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Lyra Raudales
+ * Date: 2021/08/24
+ *
+ * A simple 'Hello World' program in C.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello World!\n");
+
+    return 0;
+}
